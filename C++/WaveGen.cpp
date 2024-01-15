35caf1dfcf2365a24641a6b853c208420113c967
DreadPirate07 <tanayraikhere@gmail.com>
2024-01-15 23:41:17 +0530

    generated Wave of 440 Hz

--- C++/WaveGen.cpp
+++ C++/WaveGen.cpp
@@ -24,18 +24,49 @@ public:
     }
 };
 
+void writeToFile(std::ofstream &file, int value,int size){
+    file.write(reinterpret_cast<const char*> (&value),size);
+}
+
 int main()
 {
     int duration = 2;
     std::ofstream audioFile;
     // open the audioFile/create it.
-    audioFile.open("/home/dreadpirate/CodingPirate/C and C++/C++/waveform");
+    audioFile.open("/home/dreadpirate/CodingPirate/C and C++/C++/waveform",std::ios::binary);
     SineOscillator sineOscillator(440, 0.5);
+    
+    
+    //Header Chunk
+    audioFile<<"RIFF";
+    audioFile<<"----"; //size
+    audioFile <<"WAVE"; //format
+
+    //Format chunk
+    audioFile<<"fmt ";
+    writeToFile(audioFile,16,4);
+    writeToFile(audioFile,1,2); // compression code
+    writeToFile(audioFile,1,2); //Number of Channels
+    writeToFile(audioFile,sampleRate,4);
+    writeToFile(audioFile,sampleRate*bitDepth/8,4); //Byte sampleRate
+    writeToFile(audioFile,2,2); // Block align
+    writeToFile(audioFile,bitDepth,2);
+
+    //Data chunk
+    audioFile <<"data";
+    audioFile << "----";
+
+
+
     // loop runs for the time duration we want the audio
     for (int i = 0; i < (sampleRate * duration); i++)
     {
-        //streams data into audioFile
-        audioFile << sineOscillator.process() << std::endl;
+        // //streams data into audioFile
+        // audioFile << sineOscillator.process() << std::endl;
+
+        auto sample = sineOscillator.process();
+        int intsample = static_cast<int>(sample * 32767);
+        writeToFile(audioFile,intsample,2);
     }
     audioFile.close();
     return 0;
