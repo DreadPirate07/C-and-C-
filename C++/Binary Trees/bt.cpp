47ff4f057e36a25c0d37a35b06f07bc38aae1cfc
DreadPirate07 <tanayraikhere@gmail.com>
2024-02-21 10:59:55 +0530

    Learnt inOrder PreOrder and PostOrder Traversal

--- C++/Binary Trees/bt.cpp
+++ C++/Binary Trees/bt.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <vector>
 
 class binaryTree
 {
@@ -19,16 +20,70 @@ public:
 binaryTree *initTree(binaryTree *root)
 {
     int x;
-
-    std::cout << "Enter the value: ";
     std::cin >> x;
     if (x < 0)
         return NULL;
     root = new binaryTree(x);
-    std::cout << "Currently at node value:" << root->data << std::endl;
+    std::cout << "Currently at node value: " << root->data << std::endl;
+    std::cout << "Enter the left data to " << x;
     root->left = initTree(root->left);
+    std::cout << "Enter the right data to " << x;
     root->right = (initTree(root->right));
     return root;
+}
+
+void levelOrderTraversal(binaryTree *node)
+{
+    std::queue<binaryTree *> q;
+    q.push(node);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        binaryTree *front = q.front();
+        if (front != NULL)
+            std::cout << " " << front->data << " ";
+        q.pop();
+
+        if (front->left != NULL)
+        {
+            q.push(front->left);
+            q.push(NULL);
+        }
+        if (front->right != NULL)
+        {
+            q.push(front->right);
+            q.push(NULL);
+        }
+    }
+}
+
+std::vector<std::vector<int>> levelOrder(binaryTree *node)
+{
+    std::vector<std::vector<int>> result;
+
+    std::queue<binaryTree *> q;
+    q.push(node);
+    
+
+    while (!q.empty()){
+        std::vector<int> level;
+        int levelsize = q.size();
+        
+        for (int i = 0 ; i < levelsize ; i++){
+            binaryTree *local = q.front();
+            q.pop();
+            level.push_back(local->data);
+
+            if (local->left != NULL){
+                q.push(local->left);
+            }
+            if (local->right != NULL){
+                q.push(local->right);
+            }
+            result.push_back(level);
+        }
+    }
 }
 
 int main()
